a43dc1e9d4311febef96337559b012c078bedd2b
Tomáš Jelínek <jelinekt.99@gmail.com>
2023-01-25 22:08:34 +0100

    - Project initialization using angular cli - Coding the form (inputs for: email, full name and GitHub nickname) - Adding basic functionality to build qr code using qrcode-generator repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
